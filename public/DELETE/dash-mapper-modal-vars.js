778a469a9e80f7e863ba532fa7f783079deb2bed
Matthew McNey <matt.d.mcney@gmail.com>
2025-11-14 14:54:36 -0500

    chore(legacy): delete all legacy js

--- public/DELETE/dash-mapper-modal-vars.js
+++
@@ -1,4 +0,0 @@
-(() => {
-  const panel = getComputedStyle(document.body).getPropertyValue('--dash-panel').trim() || '#E6F1FF';
-  document.documentElement.style.setProperty('--mapper-bg', panel);
-})();
